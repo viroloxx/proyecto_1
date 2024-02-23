8a996ba47d9451cb88d0142367d7fc77b8af6973
gabrielrorozcor <gorozco@correo.unimet.edu.ve>
2024-02-23 15:48:45 -0400

    Node

--- src/Node.java
+++ src/Node.java
@@ -5,8 +5,49 @@
 
 /**
  *
- * @author Diego A. Vivolo
+ * @author Diego A. Vivolo/Gabriel Orozco
  */
-public class Node {
+public class Node<T> {
+    private T data;
+    private Node<T> pNext;
+    
+    /*
+     * Metodo constructor parametrizado
+     * @param data Objeto contenido dentro del nodo
+     */
+
+    public Node(T data) {
+        this.data = data;
+        this.pNext = null;
+    }
+        
+    /**
+     * @return the data
+     */
+    public T getData() {
+        return data;
+    }
+
+    /**
+     * @param data the data to set
+     */
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    /**
+     * @return the pNext
+     */
+    public Node<T> getpNext() {
+        return pNext;
+    }
+
+    /**
+     * @param pNext the pNext to set
+     */
+    public void setpNext(Node<T> pNext) {
+        this.pNext = pNext;
+    }
+    
     
 }
